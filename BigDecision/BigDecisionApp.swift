8b088258462a0973e9eba4b612a2a2b001f973fd
Jian <riskycheng@gmail.com>
2025-03-12 21:15:59 +0800

    Initial Commit

---
+++ BigDecision/BigDecisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigDecisionApp.swift
+//  BigDecision
+//
+//  Created by Jian Cheng on 2025/3/12.
+//
+
+import SwiftUI
+
+@main
+struct BigDecisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
